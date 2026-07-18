520cf586467262aa2e112bfbc9fd828cddc1edf2
VitaliyKuzhil <vitaliy.kuzhil2018@gmail.com>
2026-07-18 15:01:21 +0300

    Added task-04 into sprint-05

---
+++ sprint-05/task-04.py
@@ -0,0 +1,24 @@
+class ToSmallNumberGroupError(Exception):
+    '''
+    Custom exception for numbers less than 10 
+    '''
+
+
+def check_number(number):
+
+    if number < 10:
+        raise ToSmallNumberGroupError('The number is too small.')
+
+    return f'Number of your group {number} is valid'
+    
+
+
+def check_number_group(group):
+    try:
+        number_of_group = check_number(int(group))
+    except ToSmallNumberGroupError:
+        return 'We obtain error:Number of your group can\'t be less than 10 '
+    except ValueError:
+        return 'You entered incorrect data. Please try again.'
+    else:
+        return number_of_group
